ef9f97435e88b61938b088efbe311027efd0868b
Dominic Humphries <dominich@broadbean.com>
2014-09-25 10:58:46 +0100

    Initial milla-created commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+App::Goto2 - Blah blah blah
+
+# SYNOPSIS
+
+    use App::Goto2;
+
+# DESCRIPTION
+
+App::Goto2 is
+
+# AUTHOR
+
+Dominic Humphries <dominic@oneandoneis2.com>
+
+# COPYRIGHT
+
+Copyright 2014- Dominic Humphries
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/App/Goto2.pm
@@ -0,0 +1,39 @@
+package App::Goto2;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Goto2 - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use App::Goto2;
+
+=head1 DESCRIPTION
+
+App::Goto2 is
+
+=head1 AUTHOR
+
+Dominic Humphries E<lt>dominic@oneandoneis2.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Dominic Humphries
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
